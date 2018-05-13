61ce9bfa817a050b3686c34445e842a9ccb72a32
Cindy <cinnndy420@outlook.es>
2018-05-12 19:44:17 -0500

    Actualizar

---
+++ Hotel.GUI.Clientes/UsuarioNuevo.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hotel.GUI.Clientes
+{
+    /// <summary>
+    /// Lógica de interacción para UsuarioNuevo.xaml
+    /// </summary>
+    public partial class UsuarioNuevo : Window
+    {
+        public UsuarioNuevo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegresar_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow ir = new MainWindow();
+            ir.Show();
+            this.Close();
+        }
+
+        private void Image_ImageFailed_1(object sender, Yunan.WPF.ImageAnim.AnimationImageExceptionRoutedEventArgs args)
+        {
+            
+        }
+    }
+}
---
+++ Hotel.GUI.Registros/TipoHabitacion.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hotel.GUI.Registros
+{
+    /// <summary>
+    /// Interaction logic for TipoHabitacion.xaml
+    /// </summary>
+    public partial class TipoHabitacion : Window
+    {
+        public TipoHabitacion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegresar_Click(object sender, RoutedEventArgs e)
+        {
+            Menu ir = new Menu();
+            ir.Show();
+            this.Close();
+        }
+    }
+}
